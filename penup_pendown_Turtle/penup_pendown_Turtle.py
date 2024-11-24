b1e4fba7ea55bb553529f37856574cde7a0f592d
Yaren Colak <yarencolak1122@outlook.com>
2024-11-24 19:30:23 +0300

    init

---
+++ penup_pendown_Turtle/penup_pendown_Turtle.py
@@ -0,0 +1,28 @@
+import turtle
+tospik = turtle.Turtle()
+tospik.shape('turtle')
+tospik.color("green")
+tospik.pensize(5)
+for i in range(4):
+    tospik.fd(160)
+    tospik.left(90)
+tospik.penup()
+tospik.goto(20,20)
+tospik.pendown()
+for i in range(4):
+    tospik.fd(120)
+    tospik.left(90)
+tospik.penup()
+tospik.goto(40,40)
+tospik.pendown()
+for i in range(4):
+    tospik.fd(80)
+    tospik.left(90)
+tospik.penup()
+tospik.goto(60,60)
+tospik.pendown()
+for i in range(4):
+    tospik.fd(40)
+    tospik.left(90)
+    
+    
